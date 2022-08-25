abdd3caaf5017b7d6a4994bb598083b5db106303
Artemiza Rocha <88461178+Mizarocha@users.noreply.github.com>
2022-08-25 12:42:47 -0300

    react na prática
    Aplicando pros e componets

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react"
+
+class App extends Component{
+ 
+ constructor(props){
+  super(props);
+  this.state = {
+
+    nome: 'Artemiza',
+    contador:0
+  }; 
+    
+    this.aumentar = this.aumentar.bind(this);
+    this.diminuir = this.diminuir.bind(this);
+}
+ 
+aumentar(){
+  let state = this.state;
+  state.contador += 1;
+  this.setState(state);
+}
+ 
+ diminuir(){
+  let state = this.state;
+  if(state.contador === 0){
+    alert('Opa chegou a zero!');
+    return;
+  }
+  state.contador -= 1;
+  this.setState(state);
+
+ } 
+ 
+  render(){
+  return(
+    <div>
+      <h1>Contador</h1>
+      {this.state.nome}
+
+      <h3>
+        <button onClick={this.diminuir}>-</button>
+        {this.state.contador}
+        <button onClick={this.aumentar}>+</button>
+        
+      </h3>
+    
+    </div>
+
+  );
+ }
+
+}
+
+
+export default App;
